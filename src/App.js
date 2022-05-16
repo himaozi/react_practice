51f009099886f8dc99ce579c1f04f81718260291
ylb <funnymaozi@gmail.com>
2022-05-17 01:05:16 +0800

    删除无用代码

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { getBookList, getPagedBooksList, queryBookByID } from './api';
 import React from 'react';
 import BookLists from './Components/Mytable';
 class App extends React.Component {
@@ -8,18 +7,6 @@ class App extends React.Component {
 
     this.state = {};
 
-  }
-  componentDidMount() {
-    // 请求接口一般在组件的 componentDidMount 周期函数里
-    getBookList().then(data => {
-      console.log(1, data)
-    })
-    getPagedBooksList(1, 5).then(data => {
-      console.log(2, data)
-    })
-    queryBookByID(244).then(data => {
-      console.log(3, data)
-    })
   }
 
   render() {
